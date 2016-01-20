cfc490d3ed3a5586885bb4f39db3c30a4fea6203
stefan-endres <stefan.c.endres@gmail.com>
2016-01-20 21:09:40 +0200

    Fix tgo_test sub-func tests 4 and 5.
    Tests had duplicate names causing them not to run, picked up with coverage and fixed error in K_optimal test.

---
+++ data_handling_tests.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Test Run examples:
+
+ex.
+$ python2 -m unittest -v data_handling_tests.TestTgoFuncs
+$ python2 -m unittest -v data_handling_tests.TestTgoSubFuncs
+"""
+import data_handling
+import unittest
+
+class TestDataLoad(object):
